f9e351fd2d92a44c846ffd0b0dd00cc379dab5c6
sposeidonc <sckuttu@gmail.com>
2023-04-22 18:31:40 +0530

    Add files via upload
    Commiting to add the first draft of code.

---
+++ index.js
@@ -0,0 +1,28 @@
+// Setting default username and pw
+const usernameData = "admin";
+localStorage.setItem("Username" , usernameData);
+
+const passwordData = "test123";
+localStorage.setItem("Password" , passwordData);
+
+// Function to validate the username and password
+function check(){
+    var storedName = localStorage.getItem('Username');
+    var storedPw = localStorage.getItem('Password');
+
+    var userName = document.getElementById('username');
+    var userPw = document.getElementById('password');
+
+    if(userName.value == storedName && userPw.value == storedPw){
+
+        alert('You are logged in.');
+    }else{
+        alert('Invalid username/password');
+        document.getElementById("login-page-container").innerHTML = 
+        `<form action = "./index.html">
+            <input id="username" type="text" placeholder="User Name">
+            <input id="password" type="password" placeholder="Password">
+            <input id="login-button" type="button" value="Login" style="font-size: x-large;" onclick="check()">
+        </form>`
+    }
+}
